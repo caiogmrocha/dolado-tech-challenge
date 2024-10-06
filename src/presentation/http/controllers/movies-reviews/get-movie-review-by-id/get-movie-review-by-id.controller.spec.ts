52594c8a2ead3675084f2c0cde2ec8fbbf95bd9f
caiogmrocha <caioguilherme739@gmail.com>
2024-10-05 23:40:15 -0300

    test: ensure GetMovieReviewByIdController return movie review by id

--- src/presentation/http/controllers/movies-reviews/get-movie-review-by-id/get-movie-review-by-id.controller.spec.ts
+++ src/presentation/http/controllers/movies-reviews/get-movie-review-by-id/get-movie-review-by-id.controller.spec.ts
@@ -1,8 +1,16 @@
-import { MovieReviewsRepository } from "@/app/interfaces/repositories/movie-reviews.repository";
-import { GetMovieReviewByIdService } from "@/app/services/movies-reviews/get-movie-review-by-id/get-movie-review-by-id.service";
 import { Test, TestingModule } from "@nestjs/testing";
 
+import { faker } from "@faker-js/faker";
+
+import { GetMovieReviewByIdController } from "./get-movie-review-by-id.controller";
+import { GetMovieReviewByIdService } from "@/app/services/movies-reviews/get-movie-review-by-id/get-movie-review-by-id.service";
+import { MovieReviewsRepository } from "@/app/interfaces/repositories/movie-reviews.repository";
+import { MovieReview } from "@/domain/entities/movie-review.entity";
+import { Movie } from "@/domain/entities/movie.entity";
+import { Author } from "@/domain/entities/author.entity";
+
 describe('[Unit] GetMovieReviewByIdController', () => {
+  let controller: GetMovieReviewByIdController;
   let service: GetMovieReviewByIdService;
   let moviesReviewsRepository: jest.Mocked<MovieReviewsRepository>;
 
@@ -17,12 +25,42 @@ describe('[Unit] GetMovieReviewByIdController', () => {
         },
         GetMovieReviewByIdService,
       ],
+      controllers: [GetMovieReviewByIdController],
     }).compile();
 
+    controller = module.get<GetMovieReviewByIdController>(GetMovieReviewByIdController);
     service = module.get<GetMovieReviewByIdService>(GetMovieReviewByIdService);
     moviesReviewsRepository = module.get<jest.Mocked<MovieReviewsRepository>>(MovieReviewsRepository);
   });
 
-  it.todo('should return movie review by id');
+  it('should return movie review by id', async () => {
+    // Arrange
+    const id = faker.number.int();
+    const movieReview = new MovieReview({
+      movie: new Movie({
+        title: faker.lorem.words(),
+        rating: faker.number.float(),
+        releasedAt: faker.date.recent(),
+        authors: Array.from({ length: faker.number.int({ min: 1, max: 5 }) }, () => new Author({
+          name: faker.person.fullName(),
+        })),
+      }),
+      notes: faker.lorem.paragraph(),
+    });
+    moviesReviewsRepository.getById.mockResolvedValue(movieReview);
+
+    // Act
+    const response = await controller.handle({ id });
+
+    // Assert
+    expect(response).toEqual({
+      title: movieReview.movie.title,
+      rating: movieReview.movie.rating,
+      releasedAt: movieReview.movie.releasedAt,
+      notes: movieReview.notes,
+      authors: movieReview.movie.authors.map(author => author.name),
+    });
+  });
+
   it.todo('should throw NotFoundException when movie review not found');
 });
