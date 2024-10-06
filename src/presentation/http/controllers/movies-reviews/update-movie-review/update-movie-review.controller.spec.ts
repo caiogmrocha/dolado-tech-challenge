6afc32f2cdd219587ed3717fb10700bd896f60e9
caiogmrocha <caioguilherme739@gmail.com>
2024-10-06 12:11:13 -0300

    test: ensure UpdateMovieReviewController to be able to update movie review

--- src/presentation/http/controllers/movies-reviews/update-movie-review/update-movie-review.controller.spec.ts
+++ src/presentation/http/controllers/movies-reviews/update-movie-review/update-movie-review.controller.spec.ts
@@ -6,6 +6,7 @@ import { faker } from "@faker-js/faker";
 import { UpdateMovieReviewController } from "./update-movie-review.controller";
 import { UpdateMovieReviewService } from "@/app/services/movies-reviews/update-movie-review/update-movie-review.service";
 import { MovieReviewsRepository } from "@/app/interfaces/repositories/movie-reviews.repository";
+import { MovieReview } from "@/domain/entities/movie-review.entity";
 
 describe('[Unit] UpdateMovieReviewController', () => {
   let controller: UpdateMovieReviewController;
@@ -32,15 +33,34 @@ describe('[Unit] UpdateMovieReviewController', () => {
     moviesReviewsRepository = module.get<jest.Mocked<MovieReviewsRepository>>(MovieReviewsRepository);
   });
 
-  it.todo('should update movie review');
+  it('should update movie review', async () => {
+    // Arrange
+    const params = { id: 1 };
+    const body = { notes: faker.lorem.paragraph() };
+
+    const movieReview = new MovieReview({
+      id: params.id,
+      notes: faker.lorem.paragraph(),
+    });
+
+    moviesReviewsRepository.getById.mockResolvedValue(movieReview);
+
+    // Act
+    await controller.handle({ id: params.id }, body);
+
+    // Assert
+    expect(moviesReviewsRepository.getById).toHaveBeenCalledWith(params.id);
+    expect(moviesReviewsRepository.update).toHaveBeenCalledWith({ id: params.id, notes: body.notes });
+  });
 
   it('should throw NotFoundException when movie review not found', async () => {
     // Arrange
-    const id = 1;
+    const params = { id: 1 };
+
     moviesReviewsRepository.getById.mockResolvedValue(null);
 
     // Act
-    const promise = controller.handle({ id }, { notes: faker.lorem.paragraph() });
+    const promise = controller.handle({ id: params.id }, { notes: faker.lorem.paragraph() });
 
     // Assert
     await expect(promise).rejects.toThrow(NotFoundException);
