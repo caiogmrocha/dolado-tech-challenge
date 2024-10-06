67b3f2e9f3680bce00618f9ce9aa1b4214a5b4e6
caiogmrocha <caioguilherme739@gmail.com>
2024-10-05 23:29:43 -0300

    test: ensure GetMovieReviewByIdService return movie review by id

--- src/app/services/movies-reviews/get-movie-review-by-id/get-movie-review-by-id.service.spec.ts
+++ src/app/services/movies-reviews/get-movie-review-by-id/get-movie-review-by-id.service.spec.ts
@@ -4,6 +4,9 @@ import { faker } from "@faker-js/faker";
 import { MovieReviewsRepository } from "@/app/interfaces/repositories/movie-reviews.repository";
 import { GetMovieReviewByIdService } from "./get-movie-review-by-id.service";
 import { MovieReviewNotFoundException } from "../errors/movie-review-not-found.exception";
+import { MovieReview } from "@/domain/entities/movie-review.entity";
+import { Movie } from "@/domain/entities/movie.entity";
+import { Author } from "@/domain/entities/author.entity";
 
 describe('[Unit] GetPaginatedMovieReviewsService', () => {
   let service: GetMovieReviewByIdService;
@@ -26,7 +29,34 @@ describe('[Unit] GetPaginatedMovieReviewsService', () => {
     moviesReviewsRepository = module.get<jest.Mocked<MovieReviewsRepository>>(MovieReviewsRepository);
   });
 
-  it.todo('should return movie review by id');
+  it('should return movie review by id', async () => {
+    // Arrange
+    const id = faker.number.int();
+    const movieReview = new MovieReview({
+      movie: new Movie({
+        title: faker.lorem.words(),
+        rating: faker.number.float(),
+        releasedAt: faker.date.recent(),
+        authors: Array.from({ length: faker.number.int({ min: 1, max: 5 }) }, () => new Author({
+          name: faker.person.fullName(),
+        })),
+      }),
+      notes: faker.lorem.paragraph(),
+    });
+    moviesReviewsRepository.getById.mockResolvedValue(movieReview);
+
+    // Act
+    const response = await service.execute({ id });
+
+    // Assert
+    expect(response).toEqual({
+      title: movieReview.movie.title,
+      rating: movieReview.movie.rating,
+      releasedAt: movieReview.movie.releasedAt,
+      notes: movieReview.notes,
+      authors: movieReview.movie.authors.map(author => author.name),
+    });
+  });
 
   it('should throw MovieReviewNotFoundException when movie review not found', async () => {
     // Arrange
