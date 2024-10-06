49d67f96db4f5f31ffa3e3ce7354694c706d16e2
caiogmrocha <caioguilherme739@gmail.com>
2024-10-06 12:38:13 -0300

    feat: ensure DeleteMovieReviewService throws MovieReviewNotFoundException when movie review does not exist

--- src/app/services/movies-reviews/delete-movie-review/delete-movie-review.service.ts
+++ src/app/services/movies-reviews/delete-movie-review/delete-movie-review.service.ts
@@ -1,5 +1,10 @@
 import { MovieReviewsRepository } from "@/app/interfaces/repositories/movie-reviews.repository";
 import { Inject, Injectable } from "@nestjs/common";
+import { MovieReviewNotFoundException } from "../errors/movie-review-not-found.exception";
+
+export type DeleteMovieReviewServiceParams = {
+  id: number;
+}
 
 @Injectable()
 export class DeleteMovieReviewService {
@@ -7,7 +12,11 @@ export class DeleteMovieReviewService {
     @Inject(MovieReviewsRepository) private readonly movieReviewsRepository: MovieReviewsRepository
   ) {}
 
-  public async execute(movieReviewId: string): Promise<void> {
-    return;
+  public async execute(params: DeleteMovieReviewServiceParams): Promise<void> {
+    const movieReview = await this.movieReviewsRepository.getById(params.id);
+
+    if (!movieReview) {
+      throw new MovieReviewNotFoundException(params.id);
+    }
   }
 }
