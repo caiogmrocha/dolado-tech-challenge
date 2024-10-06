1058a6f3965821b22cfefa14d5f20ae1182d8735
caiogmrocha <caioguilherme739@gmail.com>
2024-10-06 11:58:44 -0300

    test: ensure UpdateMovieReviewService to be able to update movie review

--- src/app/services/movies-reviews/update-movie-review/update-movie-review.service.spec.ts
+++ src/app/services/movies-reviews/update-movie-review/update-movie-review.service.spec.ts
@@ -3,6 +3,7 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { UpdateMovieReviewService } from "./update-movie-review.service";
 import { MovieReviewsRepository } from "@/app/interfaces/repositories/movie-reviews.repository";
 import { MovieReviewNotFoundException } from "../errors/movie-review-not-found.exception";
+import { MovieReview } from "@/domain/entities/movie-review.entity";
 
 describe('[Unit] UpdateMovieReviewService', () => {
   let service: UpdateMovieReviewService;
@@ -15,6 +16,7 @@ describe('[Unit] UpdateMovieReviewService', () => {
           provide: MovieReviewsRepository,
           useClass: jest.fn().mockImplementation(() => ({
             getById: jest.fn(),
+            update: jest.fn(),
           })),
         },
         UpdateMovieReviewService,
@@ -25,7 +27,22 @@ describe('[Unit] UpdateMovieReviewService', () => {
     moviesReviewsRepository = module.get<jest.Mocked<MovieReviewsRepository>>(MovieReviewsRepository);
   });
 
-  it.todo('should update movie review');
+  it('should update movie review', async () => {
+    // Arrange
+    const id = 1;
+    const movieReview = new MovieReview({
+      id,
+      notes: 'notes',
+    });
+    moviesReviewsRepository.getById.mockResolvedValue(movieReview);
+
+    // Act
+    await service.execute({ id, notes: 'new notes' });
+
+    // Assert
+    expect(moviesReviewsRepository.getById).toHaveBeenCalledWith(id);
+    expect(moviesReviewsRepository.update).toHaveBeenCalledWith({ ...movieReview, notes: 'new notes' });
+  });
 
   it('should throw MovieReviewNotFoundException when movie review not found', async () => {
     // Arrange
