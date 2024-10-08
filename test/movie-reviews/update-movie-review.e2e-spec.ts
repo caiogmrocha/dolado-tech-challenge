bd3ffc55af3e6db34cb7d46ddb0988e33fec6603
caiogmrocha <caioguilherme739@gmail.com>
2024-10-07 22:06:24 -0300

    test(e2e): map e2e test cases to UpdateMovieReviewController

---
+++ test/movie-reviews/update-movie-review.e2e-spec.ts
@@ -0,0 +1,71 @@
+import { HttpStatus, INestApplication, ValidationPipe } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { Test, TestingModule } from "@nestjs/testing";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+import * as request from 'supertest';
+import { DataSource } from "typeorm";
+
+import { MovieReviewsModule } from "@/main/movie-reviews.module";
+import { GetDatabaseConnectionSingleton } from "../utils/create-database-connection";
+
+describe('UpdateMovieReviewController (e2e)', () => {
+  let app: INestApplication;
+  let datasource: DataSource;
+  let database: string;
+
+  beforeAll(async () => {
+    await Test.createTestingModule({
+      imports: [ConfigModule.forRoot({ envFilePath: '.env.test' })],
+    }).compile();
+
+    database = `test_${crypto.randomUUID()}`.replaceAll('-', '_').toLowerCase();
+
+    datasource = await GetDatabaseConnectionSingleton.getInstance();
+  });
+
+  beforeEach(async () => {
+    await datasource.query('CREATE DATABASE ' + database);
+
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: process.env.DB_HOST,
+          port: process.env.DB_PORT,
+          username: process.env.DB_USER,
+          password: process.env.DB_PASS,
+          database: database,
+          ssl: false,
+          entities: [
+            __dirname + '/../../src/domain/entities/*.entity.{ts,js}',
+          ],
+          synchronize: true,
+        }),
+        MovieReviewsModule,
+      ],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+
+    app.useGlobalPipes(new ValidationPipe({
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+      transform: true,
+    }));
+
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await datasource.query('DROP DATABASE IF EXISTS ' + database);
+  });
+
+  afterAll(async () => {
+    await datasource.destroy();
+    await app.close();
+  });
+
+  it.todo('PUT /movie-reviews/:id | should return 204 when updating a movie review');
+  it.todo('PUT /movie-reviews/:id | should return 404 when movie review does not exist');
+  it.todo('PUT /movie-reviews/:id | should return 422 when payload is invalid');
+});
